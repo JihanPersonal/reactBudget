77ff98fc426dfa1f84ba034152c05311c9c59b25
juan3juan <juan3juan@163.com>
2020-03-11 17:53:24 -0400

    Add Returnto environment

--- src/component/Auth/Auth.js
+++ src/component/Auth/Auth.js
@@ -45,7 +45,7 @@ export default class Auth {
     localStorage.removeItem("expires_at");
     this.auth0.logout({
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
-      returnTo: "http://localhost:3000"
+      returnTo: process.env.REACT_APP_AUTH0_RETURNTO_URL
     });
   };
   isAuthenticated = () => {
